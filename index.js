6393a42fec0f57dc0d3b074897cfb9593abbad13
chacka1315 <chacka131@gmail.com>
2025-07-16 21:30:05 +0000

    I wrote the screen displayer object. It permit to show all stuff on the UI. I'm going to add a button to restart new game.

--- index.js
+++ index.js
@@ -8,14 +8,14 @@ function Gameboard() {
     for (let i = 0; i < row; i++) {
         boardTable[i] = [];
         for (let j = 0; j < column; j++) {
-            boardTable[i][j] = undefined
+            boardTable[i][j] = "";
         }  
     }
 
     const getBoard = () => boardTable;
 
     const dropToken = (row, column, token) => {
-        if (boardTable[row][column] === undefined){
+        if (boardTable[row][column] === ""){
             boardTable[row][column] = token;
             return true
         } else {
@@ -48,7 +48,10 @@ function GameController(){
 
     const getActivePlayer = () => activePlayer;
 
+    //print the player turn name
+    const turn = document.querySelector(".turn")
     const printNewRound = () => {
+        turn.textContent = `It's ${getActivePlayer().name} turn`
         console.log(`It's ${getActivePlayer().name} turn`);
     }
 
@@ -98,7 +101,7 @@ function GameController(){
         const currentGameBoard = board.getBoard();
         for (let i = 0; i < 3; i++) {
             for (let j = 0; j < 3; j++){
-                if (currentGameBoard[i][j] === undefined) {
+                if (currentGameBoard[i][j] === "") {
                     return false
                 }
             }     
@@ -118,41 +121,128 @@ function GameController(){
             } else if (isDraw) {
                 return {status : "draw"}
             }
-            switchPayer();
+ 
             return {status : "continue" }
         }else {
             return {status : "invalid"}
         }
     }       
-    return{playRound, printNewRound, getActivePlayer}   
-
+    return{playRound, printNewRound, getActivePlayer, switchPayer}   
 }
 
-//lunch instance
-const board = Gameboard()
-const game = GameController();
 
+//display functions factory
+function ScreenController (){
+    const game = GameController();
+    const player1 = document.querySelector("input[id='player1']");
+    const player2 = document.querySelector("input[id='player2']");
+
+    //get all board buttons
+    const column = 3;
+    const row = 3;
+    let pageBoardTab = [];
+    for (let i = 0; i < row; i++) {
+        pageBoardTab[i] = [];
+        for (let j = 0; j < column; j++) {
+            pageBoardTab[i][j] = document.querySelector(`#btn_${i}${j}`)
+        }  
+    }
+
+    //logic that end the game
+    const stopGame = () => {
+        pageBoardTab.forEach(row => {
+            row.forEach (button => {
+                button.disabled = true
+            })
+        })
+    }
+
+    //get screen bord
+    const getPageBoardTab = () => pageBoardTab
+
+
+    //update screen after drop
+    const updateScreen = () => {
+        const currentBoard = board.getBoard()
+        const currentPageBoard = getPageBoardTab();
+
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                currentPageBoard[i][j].textContent = `${currentBoard[i][j]}` 
+            }   
+        }       
+    }
+
+
+    //select where i will display win or display message
+    const message = document.querySelector(".message");
+
+    //manage the clicks
+    pageBoardTab.forEach(row => {
+        row.forEach (button => {
+            button.addEventListener("click", () => {
+                button.disabled = true; 
+                let row = Number(button.id[4]);
+                let column = Number(button.id[5]);
+                const result = game.playRound(row, column);
+                if (result.status === "continue") {
+                    const activePlayer = game.getActivePlayer();
+                    console.log(`${activePlayer.name} drop is token ${activePlayer.token} in to row ${row}, column ${column}`);
+                    game.switchPayer();
+                    game.printNewRound();
+                }else if (result.status === "win"){
+                    board.printBoardTable();
+                    message.textContent = `${result.winner} win the game`;
+                    console.log(`${result.winner} win the game`);
+                    stopGame();
+                }else if (result.status === "draw" ) {
+                    board.printBoardTable();
+                    message.textContent = "It's DRAW !";
+                    console.log("It's DRAW !");
+                    stopGame();
+                }
+                updateScreen();
+                
+            }) 
+            
+        }) 
+           
+    }) 
+    updateScreen();
+    game.printNewRound();
+}
+
+
+
+
+
+
+
+//lunch instance
+const board = Gameboard();
+//const game = GameController();
+const gameScreen = ScreenController();
 
 //loop for a game start
-while (true) {
-    game.printNewRound();
-    board.printBoardTable();
-    x = prompt("entrez ligne")
-    y = prompt("entrez col")
-    const result = game.playRound(x, y);
-    if (result.status === "continue") {
-        const activePlayer = game.getActivePlayer();
-        console.log(`${activePlayer.name} drop is token ${activePlayer.token} in to row ${x}, column ${y}`);
-    }else if (result.status === "win"){
-        board.printBoardTable();
-        console.log(`${result.winner} win the game`);
-        break;
-    }else if (result.status === "draw" ) {
-        board.printBoardTable();
-        console.log("It's DRAW !");
-        break;
-    }
-} 
+// while (true) {
+//     game.printNewRound();
+//     board.printBoardTable();
+//     x = prompt("entrez ligne")
+//     y = prompt("entrez col")
+//     const result = game.playRound(x, y);
+//     if (result.status === "continue") {
+//         const activePlayer = game.getActivePlayer();
+//         console.log(`${activePlayer.name} drop is token ${activePlayer.token} in to row ${x}, column ${y}`);
+//     }else if (result.status === "win"){
+//         board.printBoardTable();
+//         console.log(`${result.winner} win the game`);
+//         break;
+//     }else if (result.status === "draw" ) {
+//         board.printBoardTable();
+//         console.log("It's DRAW !");
+//         break;
+//     }
+// } 
       
 
 
